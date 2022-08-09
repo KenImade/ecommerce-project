d473df530fc1410d0be28a319b34422895d5209f
KenImade <64961704+KenImade@users.noreply.github.com>
2022-08-09 21:59:58 +0100

    setup category cards

---
+++ src/components/CategoryCard/CategoryCardSection.js
@@ -0,0 +1,43 @@
+import headphoneThumbnail from "./assets/image-category-thumbnail-headphones.png";
+import earphoneThumbnail from "./assets/image-category-thumbnail-earphones.png";
+import speakerThumbnail from "./assets/image-category-thumbnail-speakers.png";
+import Button from "../Button";
+import "./assets/CategoryCardSection.css";
+
+const CategoryCardSection = () => {
+  return (
+    <section className="category-cards">
+      <div className="category-card">
+        <div className="card-img">
+          <img src={headphoneThumbnail} alt="headphone" />
+        </div>
+        <div className="bottom-card">
+          <h6>headphones</h6>
+          <Button className={"btn-three"} name={"shop"} link={"/headphones"} />
+        </div>
+      </div>
+      
+      <div className="category-card">
+        <div className="card-img">
+          <img src={speakerThumbnail} alt="speaker" />
+        </div>
+        <div className="bottom-card">
+          <h6>speakers</h6>
+          <Button className={"btn-three"} name={"shop"} link={"/speakers"} />
+        </div>
+      </div>
+
+      <div className="category-card">
+        <div className="card-img">
+          <img src={earphoneThumbnail} alt="earphone" />
+        </div>
+        <div className="bottom-card">
+          <h6>earphones</h6>
+          <Button className={"btn-three"} name={"shop"} link={"/earphones"} />
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default CategoryCardSection
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,11 +1,13 @@
 import "./assets/Home.css";
 import Advert from "../../components/Advert/Advert";
 import Hero from "./components/Hero";
+import CategoryCardSection from "../../components/CategoryCard/CategoryCardSection";
 
 const Home = () => {
   return (
     <section className="home">
       <Hero />
+      <CategoryCardSection />
       <Advert />
     </section>
   )
