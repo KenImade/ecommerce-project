5a2e51b1bd1befc50f84d1aab1aa3041238496af
KenImade <64961704+KenImade@users.noreply.github.com>
2022-08-10 18:02:10 +0100

    added horizontal line to navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -6,38 +6,39 @@ import cart from "./assets/icon-cart.svg";
 const Navbar = () => {
   return (
     <nav className="navbar">
-        <img src={logo} alt="logo" className="logo"/>
-
-        <div className="nav--links">
-            <NavLink 
-              to="/"
-              className={({isActive}) => isActive ?  'link active' : 'link'}
-            >
-              Home
-            </NavLink>
-            <NavLink 
-              to="/headphones"
-              className={({isActive}) => isActive ? 'link active' : 'link'}
-            >
-              Headphones
-            </NavLink>
-            <NavLink 
-              to="/speakers"
-              className={({isActive}) => isActive ? 'link active' : 'link'}
-            >
-              Speakers
-            </NavLink>
-            <NavLink 
-              to="/earphones"
-              className={({isActive}) => isActive ? 'link active' : 'link'}
-            >
-              Earphones
-            </NavLink>
+        <div className="container">
+          <img src={logo} alt="logo" className="logo"/>
+          <div className="nav--links">
+              <NavLink
+                to="/"
+                className={({isActive}) => isActive ?  'link active' : 'link'}
+              >
+                Home
+              </NavLink>
+              <NavLink
+                to="/headphones"
+                className={({isActive}) => isActive ? 'link active' : 'link'}
+              >
+                Headphones
+              </NavLink>
+              <NavLink
+                to="/speakers"
+                className={({isActive}) => isActive ? 'link active' : 'link'}
+              >
+                Speakers
+              </NavLink>
+              <NavLink
+                to="/earphones"
+                className={({isActive}) => isActive ? 'link active' : 'link'}
+              >
+                Earphones
+              </NavLink>
+          </div>
+          <div className="cart">
+              <img src={cart} alt="cart" />
+          </div>
         </div>
-
-        <div className="cart">
-            <img src={cart} alt="cart" />
-        </div>
+        <div className="line"></div>
     </nav>
   )
 }
