26d04a3b6f9db9a3973ea8c5df3de2d0d7371846
KenImade <64961704+KenImade@users.noreply.github.com>
2022-08-10 16:33:56 +0100

    setup home page banners

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -2,12 +2,14 @@ import "./assets/Home.css";
 import Advert from "../../components/Advert/Advert";
 import Hero from "./components/Hero";
 import CategoryCardSection from "../../components/CategoryCard/CategoryCardSection";
+import Banners from "./components/Banners";
 
 const Home = () => {
   return (
     <section className="home">
       <Hero />
       <CategoryCardSection />
+      <Banners />
       <Advert />
     </section>
   )
---
+++ src/pages/Home/components/Banners.js
@@ -0,0 +1,46 @@
+import speakerOne from "../assets/desktop/image-speaker-zx9.png";
+import speakerTwo from "../assets/desktop/image-speaker-zx7.jpg";
+import earphones from "../assets/desktop/image-earphones-yx1.jpg";
+import Button from "../../../components/Button";
+
+
+const Banners = () => {
+
+    return (
+        <div className="banners">
+        <div className="banner-one">
+          <div className="img">
+            <img src={speakerOne} alt="speaker" />
+          </div>
+          <div className="content">
+            <h1>zx9 speaker</h1>
+            <p>
+              Upgrade to premium speakers that are phenomenally built 
+              to deliver truly remarkable sound.
+            </p>
+            <Button link={"#"} className={"btn-one"} name={"see product"}/>
+          </div>
+        </div>
+
+        <div className="banner-two" style={{backgroundImage: `url(${speakerTwo})`}}>
+            <div className="content">
+                <h4>zx7 speaker</h4>
+                <Button link={"#"} className={"btn-two"} name={"see product"} />
+            </div>
+        </div>
+
+        <div className="banner-three">
+            <div className="left">
+                <img src={earphones} alt="earphones"/>
+            </div>
+
+            <div className="right">
+                <h4>yx1 earphones</h4>
+                <Button link={"#"} className={"btn-two"} name={"see product"} />
+            </div>
+        </div>
+      </div>
+    )
+}
+
+export default Banners;
